5669d31a1eed3ff44e105e090c1f0792dc1b15b2
hyperandroid <hyperandroid@gmail.com>
2016-02-16 22:57:27 +0100

    Cocos 3.6 with cpp/js/lua samples.

--- cpp/proj.android/jni/sdkbox/Sdkbox.h
+++ cpp/proj.android/jni/sdkbox/Sdkbox.h
@@ -7,7 +7,7 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V2.0.3.2"
+#define SDKBOX_VERSION_STR  "sdkbox V2.0.3.3"
 
 /**
  * Only certain compilers support __attribute__((deprecated)).
